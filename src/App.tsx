51ee4f30470a8d684b6a03e1b80f3704c8f94b92
Sergio_Mir <voidtracer6@gmail.com>
2023-10-13 11:20:28 -0400

    ADD: added the login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Login from "./pages/login"
+
 function App() {
 
   return (
     <div>
-      <h1 className='text-lg text-red-500'>Hola desde el proyecto</h1>
+      <h1 className='text-lg text-red-100'>Hola desde el proyecto</h1>
+      <Login />
     </div>
   )
 }
