8ab31f84fc3136631552891dd26ef45b72751b98
Sergio_Mir <voidtracer6@gmail.com>
2024-05-31 08:51:16 -0400

    MOD: finished order card prototype

--- src/components/home/orders/order-item.tsx
+++ src/components/home/orders/order-item.tsx
@@ -7,7 +7,7 @@ export default function OrderItem() {
         <p className='text-white font-gabarito-medium text-2xl'>
           Orden #123456
         </p>
-        <p className='text-white text-sm font-gabarito-medium pt-2'>
+        <p className='text-white text-sm font-gabarito-medium pt-5'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam placeat vel possimus odit! Vero itaque sequi voluptatem architecto omnis deserunt quibusdam quam asdfaaasdfasdfasdfasdfadsfasdf
           asdfasdfasdf
           sadfasdfasdfasdfasdf
@@ -18,11 +18,12 @@ export default function OrderItem() {
           gagjahfighafghdkghakhglkadfglkafjga
           afkjafhgkjhafkjhadfjghafdjhhic similique provident, minus rem earum facere quidem!
         </p>
-        <div className='w-[40%] mt-2 flex justify-between'>
+        <div className='w-[40%] mt-5 flex justify-between'>
           <button className='py-1 px-5 mb-3 text-white font-gabarito-bold rounded-md hover:bg-white hover:text-black transition duration-100'>Procesando</button>
           <button className='py-1 px-5 mb-3 text-white font-gabarito-bold rounded-md hover:bg-white hover:text-black transition duration-100'>Completada</button>
         </div>
       </div>
+      <div className='min-h-40 w-60 bg-gray-500 rounded-tr-lg rounded-br-lg' />
     </div>
   )
 }
