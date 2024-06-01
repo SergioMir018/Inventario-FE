8f1d8bf4df4b1b32cbc60cda9c11b09b7e60199b
Sergio_Mir <voidtracer6@gmail.com>
2024-06-01 14:01:17 -0400

    FIX: broken colors on dropdowns

--- src/components/home/orders/order-state-dropdown.tsx
+++ src/components/home/orders/order-state-dropdown.tsx
@@ -41,31 +41,31 @@ export default function OrderStateDropDown({setOrderState, order}: OrderStateDro
 
   return (
     <div ref={dropdownRef} className="flex flex-col right-0 w-full relative">
-      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-grey font-gabarito-medium hover:bg-darker focus:outline-none rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between" type="button" onClick={handleOnDropDownClick}>{order}<svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+      <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-metal font-gabarito-medium hover:bg-darker focus:outline-none rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between" type="button" onClick={handleOnDropDownClick}>{order}<svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
       </svg>
       </button>
       { isOpen && (
-        <div id="dropdown" className="bg-white divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700
-        absolute pointer bottom-10 animate-fade-up animate-duration-100 animate-ease-in-out">
+        <div id="dropdown" className="bg-white divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-metal
+        absolute pointer bottom-12 animate-fade-up animate-duration-100 animate-ease-in-out">
           <ul className="py-2 text-sm text-gray-700 dark:text-gray-200 font-gabarito-medium" aria-labelledby="dropdownDefaultButton">
             <li>
-              <div onClick={() => handleDropdownClick("En espera")} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer">
+              <div onClick={() => handleDropdownClick("En espera")} className="block px-4 py-2 hover:bg-gray-500/30 dark:hover:bg-black/30 dark:hover:text-white cursor-pointer">
                 En espera
               </div>
             </li>
             <li>
-              <div onClick={() => handleDropdownClick("Procesando")} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer">
+              <div onClick={() => handleDropdownClick("Procesando")} className="block px-4 py-2 hover:bg-gray-500/30 dark:hover:bg-black/30 dark:hover:text-white cursor-pointer">
                 Procesando
               </div>
             </li>
             <li>
-              <div onClick={() => handleDropdownClick("Enviada")} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer">
+              <div onClick={() => handleDropdownClick("Enviada")} className="block px-4 py-2 hover:bg-gray-500/30 dark:hover:bg-black/30 dark:hover:text-white cursor-pointer">
                 Enviada
               </div>
             </li>
             <li>
-              <div onClick={() => handleDropdownClick("Completada")} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer">
+              <div onClick={() => handleDropdownClick("Completada")} className="block px-4 py-2 hover:bg-gray-500/30 dark:hover:bg-black/30 dark:hover:text-white cursor-pointer">
                 Completada 
               </div>
             </li>
