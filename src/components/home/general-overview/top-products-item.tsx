17344418ba97466e03975515485eb34fd8338b09
Sergio_Mir <voidtracer6@gmail.com>
2024-06-01 12:45:38 -0400

    ADD: added top 10 products table

---
+++ src/components/home/general-overview/top-products-item.tsx
@@ -0,0 +1,11 @@
+export default function TopProductsItem() {
+  return (
+    <div className="w-full grid grid-cols-5 font-gabarito mt-3 border-b pb-1">
+      <p className="pb-1">Electro</p>
+      <p className="text-end pb-2">Laptop</p>
+      <p className="text-end pb-2">$10</p>
+      <p className="text-end pb-2">4</p>
+      <p className="text-end pb-2">$80</p>
+    </div>
+  )
+}
