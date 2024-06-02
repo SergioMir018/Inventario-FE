fadb81a6ea88b7d1cf13c0c3696dd315900cec9b
Sergio_Mir <voidtracer6@gmail.com>
2024-06-02 11:05:22 -0400

    MOD: enhanced top products table styles

--- src/components/home/general-overview/top-products-categories.tsx
+++ src/components/home/general-overview/top-products-categories.tsx
@@ -1,11 +1,11 @@
 export default function TopProductsCategories() {
   return (
     <div className="w-full grid grid-cols-5 font-gabarito mt-3 border-b py-2">
-      <p className="">Categoría</p>
-      <p className="text-end mr-5">Nombre</p>
-      <p className="text-end mr-5">Precio</p>
-      <p className="text-end mr-5">Total de compras</p>
-      <p className="text-end mr-8">Ganancias totales</p>
+      <p className="pb-1">Categoría</p>
+      <p className="text-end pb-1">Nombre</p>
+      <p className="text-end pb-1">Precio</p>
+      <p className="text-end pb-1">Total de compras</p>
+      <p className="text-end pb-1">Ganancias totales</p>
     </div>
   )
 }
--- src/components/home/general-overview/top-products-item.tsx
+++ src/components/home/general-overview/top-products-item.tsx
@@ -1,11 +1,11 @@
 export default function TopProductsItem() {
   return (
-    <div className="w-full grid grid-cols-5 font-gabarito mt-3 border-b pb-1">
-      <p className="pb-1">Electro</p>
-      <p className="text-end pb-2">Laptop</p>
-      <p className="text-end pb-2">$10</p>
-      <p className="text-end pb-2">4</p>
-      <p className="text-end pb-2">$80</p>
+    <div className="w-full grid grid-cols-5 font-gabarito mt-3 border-b-2 border-white/10 pb-1">
+      <p className="pt-2 pb-3">Electro</p>
+      <p className="text-end pt-2 pb-3">Laptop</p>
+      <p className="text-end pt-2 pb-3">$10</p>
+      <p className="text-end pt-2 pb-3">4</p>
+      <p className="text-end pt-2 pb-3">$80</p>
     </div>
   )
 }
