45578b0bc6c31c29eb7aed3509a2a96a5ccb3f1a
Sergio_Mir <voidtracer6@gmail.com>
2024-06-04 11:54:20 -0400

    MOD: removed unnecessary dependencies and changed some Links for NavLinks

--- src/components/shared/admin-menu.tsx
+++ src/components/shared/admin-menu.tsx
@@ -1,46 +1,38 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import ChartIcon from "../../icons/chart-icon";
 import OrderIcon from "../../icons/order-icon";
 import ShoppingBagIcon from "../../icons/shopping-bag-icon";
-import { useState } from "react";
 import classNames from "classnames";
 
 export default function AdminMenu() {
-
-  const [selectedItem, setSelectedItem] = useState<string>();
-
-  const changeSelectedItem = (item: string) => {
-    setSelectedItem(item);
-  }
-
   return (
     <section className="flex flex-col border-t-2 border-b-2 border-white/10
       rounded-tr-3xl pb-[80%] rounded-br-3xl pt-3 items-center">
       <ul className="flex flex-col gap-2 font-gabarito-medium text-lg 
         text-white w-full pr-4">
-        <li className={classNames("rounded-lg", {
-          "bg-white text-black": selectedItem === "Descripción general",
-          "hover:bg-white/80 hover:text-black transition duration-100": selectedItem !== "Descripción general",
-        })}>
-          <Link to={'overview'} onClick={() => changeSelectedItem('Descripción general')} className="flex flex-row items-center w-full py-2">
+        <li>
+          <NavLink to={'overview'} className={({isActive}) => classNames("flex flex-row items-center w-full py-2 rounded-lg", {
+            "bg-white text-black": isActive,
+            "hover:bg-white/80 hover:text-black transition duration-100": !isActive,
+          })}>
             <ChartIcon /> Descripción general
-          </Link>
+          </NavLink>
         </li>
-        <li className={classNames("rounded-lg", {
-          "bg-white text-black": selectedItem === "Productos",
-          "hover:bg-white/80 hover:text-black transition duration-100": selectedItem !== "Productos",
-        })}>
-          <Link to={'products'} onClick={() => changeSelectedItem('Productos')} className="flex flex-row items-center w-full py-2">
+        <li>
+          <NavLink to={'products'} className={({ isActive }) => classNames("flex flex-row items-center w-full py-2 rounded-lg", {
+            "bg-white text-black": isActive,
+            "hover:bg-white/80 hover:text-black transition duration-100": !isActive,
+          })}>
             <ShoppingBagIcon /> Productos 
-          </Link> 
+          </NavLink> 
         </li>
-        <li className={classNames("rounded-lg", {
-          "bg-white text-black": selectedItem === "Ordenes",
-          "hover:bg-white/80 hover:text-black transition duration-100": selectedItem !== "Ordenes",
-        })}>
-          <Link to={'orders'} onClick={() => changeSelectedItem('Ordenes')} className="flex flex-row items-center w-full py-2">
+        <li>
+          <NavLink to={'orders'} className={({ isActive }) => classNames("flex flex-row items-center w-full py-2 rounded-lg", {
+            "bg-white text-black": isActive,
+            "hover:bg-white/80 hover:text-black transition duration-100": !isActive,
+          })}>
             <OrderIcon /> Ordenes
-          </Link> 
+          </NavLink> 
         </li>
       </ul>
     </section>
