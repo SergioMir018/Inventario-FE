73ead3f549ed19b29829893a69b8c0d896456bf5
Sergio_Mir <voidtracer6@gmail.com>
2024-06-15 16:02:16 -0400

    ADD: new filters for product search

--- src/components/client-home/shop/shop.tsx
+++ src/components/client-home/shop/shop.tsx
@@ -6,12 +6,19 @@ import { Product } from '../../../types/http-types';
 
 export default function Shop() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+
+  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
+  const [minPrice, setMinPrice] = useState<number | null>(null);
+  const [maxPrice, setMaxPrice] = useState<number | null>(null);
+  const [productName, setProductName] = useState<string>('');
 
   useEffect(() => {
     const getProducts = async () => {
       try {
         const productsData = await fetchProducts();
         setProducts(productsData);
+        setFilteredProducts(productsData);
       } catch (error) {
         console.error(error);
       }
@@ -19,15 +26,47 @@ export default function Shop() {
 
     getProducts();
   }, []);
+
+  const searchWithFilters = () => {
+    let filtered = products;
+
+    if (selectedCategory.length > 0) {
+      filtered = filtered.filter(product => selectedCategory.includes(product.category));
+    }
+
+    if (minPrice !== null) {
+      filtered = filtered.filter(product => product.price >= minPrice);
+    }
+
+    if (maxPrice !== null) {
+      filtered = filtered.filter(product => product.price <= maxPrice);
+    }
+
+    if (productName) {
+      filtered = filtered.filter(product =>
+        product.name.toLowerCase().includes(productName.toLowerCase())
+      );
+    }
+
+    setFilteredProducts(filtered);
+  };
+
   return (
     <section className='text-white font-gabarito grid grid-cols-4 mx-28 mt-12 h-full mb-auto'>
       <div className='col-span-1 flex justify-center items-start'>
-        <Filters />
+        <Filters
+          filterAction={searchWithFilters}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          setMinPrice={setMinPrice}
+          setMaxPrice={setMaxPrice}
+          setProductName={setProductName}
+        />
       </div>
       <div className='col-span-3 mb-10'>
         <h1 className='col-span-3 font-gabarito-bold text-5xl mb-5'>Tienda</h1>
         <div className='grid grid-cols-3 gap-4'>
-          {products.map((product) => (
+          {filteredProducts.map((product) => (
             <ShopProductsItem
               key={product.id}
               product={product}
